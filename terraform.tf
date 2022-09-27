3440f32abcc899f3c8e8c75cd0c469a96f79ed31
kurves <kavssimply@gmail.com>
2022-09-27 23:21:16 +0300

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  /*
+  cloud {
+    workspaces {
+      name = "terraform-dependencies"
+    }
+  }
+  */
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.17.1"
+    }
+  }
+
+  required_version = ">= 1.2"
+}
